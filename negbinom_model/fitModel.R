eedb24d8db3164f2b8cce835c0c809df20554f73
Andreas Kryger Jensen <aeje@sund.ku.dk>
2020-08-07 17:49:50 +0200

    daily update

--- negbinom_model/fitModel.R
+++ negbinom_model/fitModel.R
@@ -30,6 +30,15 @@ sDat <- list(N = nrow(d),
 
 samps <- sampling(m, data = sDat, iter = 10000, seed = 12345, chains = 4, cores = 4)
 
+mu_post <- extract(samps, "mu")$mu
+y_pred <- extract(samps, "yPred")$yPred
+
+df <- t(apply(mu_post, 1, function(f) {
+  predict(smooth.spline(tObs, f, all.knots = TRUE), deriv=1)$y
+}))
+
+tdi <- apply(df, 2, function(q) mean(q > 0))
+
 save.image(file = "DK-COVID19_negbinom.RData") #too big for GitHub
 
 #Trace plot for hyper-parameters
@@ -39,9 +48,6 @@ save.image(file = "DK-COVID19_negbinom.RData") #too big for GitHub
 #
 ########################################################
 load("DK-COVID19_negbinom.RData")
-
-mu_post <- extract(samps, "mu")$mu
-y_pred <- extract(samps, "yPred")$yPred
 
 band <- function(t, l, u, col) {
   polygon(c(t, rev(t)), c(l, rev(u)), col=col, border = NA)
@@ -73,13 +79,6 @@ legend("topleft",
        lty = c(1, NA, NA), pch = c(NA, 15, 15), pt.cex=1.5)
 dev.off()
 
-df <- t(apply(mu_post, 1, function(f) {
-  predict(smooth.spline(tObs, f, all.knots = TRUE), deriv=1)$y
-}))
-
-tdi <- apply(df, 2, function(q) mean(q > 0))
-
-
 pdf("../figures/negbinom_fig2.pdf", width = 12, height = 6)
 par(mfrow=c(1, 2), bty = "n", mar = c(2.3, 2.3, 1, 0.5), mgp=c(1.3,0.4,0))
 
@@ -105,3 +104,67 @@ text(max(tObs), round(tdi[length(tObs)]*100, 2),
 
 dev.off()
 
+##########################
+pdf("../figures/negbinom_fig3.pdf", width = 12, height = 8)
+par(bty = "n", mar = c(2.3, 2.3, 1, 0), mgp=c(1.3,0.4,0))
+layout(matrix(c(1,2,3,3), 2, 2, byrow=TRUE))
+
+plot(tObs, sDat$y, ylim=c(0,120), type="n",
+     xaxt="n", xlab="Number of days since March 1th 2020",
+     ylab="Number", xlim=c(0,168))
+lines(rep(as.Date("2020-04-01") - as.Date("2020-03-01"), 2), c(0, 120), lty=3, col="gray50")
+lines(rep(as.Date("2020-05-01") - as.Date("2020-03-01"), 2), c(0, 120), lty=3, col="gray50")
+lines(rep(as.Date("2020-06-01") - as.Date("2020-03-01"), 2), c(0, 120), lty=3, col="gray50")
+lines(rep(as.Date("2020-07-01") - as.Date("2020-03-01"), 2), c(0, 120), lty=3, col="gray50")
+lines(rep(as.Date("2020-08-01") - as.Date("2020-03-01"), 2), c(0, 120), lty=3, col="gray50")
+
+axis(1, seq(0, 168, 14), cex.axis=0.95)
+band(tObs, 
+     apply(y_pred, 2, quantile, 0.025),
+     apply(y_pred, 2, quantile, 0.975),
+     col = "gray90")
+band(tObs, 
+     apply(mu_post, 2, quantile, 0.025),
+     apply(mu_post, 2, quantile, 0.975),
+     col = "gray65")
+lines(tObs, apply(mu_post, 2, mean), lwd=2)
+points(tObs, sDat$y, pch=19, cex=0.5)
+title("Daily hospital admissions", font.main=1)
+legend("topleft",
+       c("Posterior mean", "95% CI", "95% posterior prediction interval"), 
+       col = c("black", "gray65", "gray90"), lwd = 2, bty="n", cex=0.7, 
+       lty = c(1, NA, NA), pch = c(NA, 15, 15), pt.cex=1.5)
+
+plot(tObs, sDat$y, ylim=c(-6,8), type="n",
+     xaxt="n", xlab="Number of days since March 1th 2020",
+     ylab="Slope", xlim=c(0,168))
+lines(rep(as.Date("2020-04-01") - as.Date("2020-03-01"), 2), c(-6, 8), lty=3, col="gray50")
+lines(rep(as.Date("2020-05-01") - as.Date("2020-03-01"), 2), c(-6, 8), lty=3, col="gray50")
+lines(rep(as.Date("2020-06-01") - as.Date("2020-03-01"), 2), c(-6, 8), lty=3, col="gray50")
+lines(rep(as.Date("2020-07-01") - as.Date("2020-03-01"), 2), c(-6, 8), lty=3, col="gray50")
+lines(rep(as.Date("2020-08-01") - as.Date("2020-03-01"), 2), c(-6, 8), lty=3, col="gray50")
+
+axis(1, seq(0, 168, 14), cex.axis=0.90)
+band(tObs, 
+     apply(df, 2, quantile, 0.025),
+     apply(df, 2, quantile, 0.975),
+     col = "gray65")
+lines(tObs, apply(df, 2, mean), lwd=2)
+lines(c(0,168), c(0,0), lty=2)
+title("Change in hospital admissions", font.main=1)
+
+plot(tObs, tdi * 100, ylim=c(0,100), type="l",
+     xaxt="n", xlab="Number of days since March 1th 2020",
+     ylab="Trend Direction Index [%]", lwd=2, xlim=c(0,168))
+axis(1, seq(0, 168, 14), cex.axis=0.90)
+lines(c(0,168), c(50,50), lty=2)
+text(max(tObs), round(tdi[length(tObs)]*100, 2), 
+     paste(round(tdi[length(tObs)]*100, 2), "%", sep=""), pos=3, cex=0.7)
+
+lines(rep(as.Date("2020-04-01") - as.Date("2020-03-01"), 2), c(0, 100), lty=3, col="gray50")
+lines(rep(as.Date("2020-05-01") - as.Date("2020-03-01"), 2), c(0, 100), lty=3, col="gray50")
+lines(rep(as.Date("2020-06-01") - as.Date("2020-03-01"), 2), c(0, 100), lty=3, col="gray50")
+lines(rep(as.Date("2020-07-01") - as.Date("2020-03-01"), 2), c(0, 100), lty=3, col="gray50")
+lines(rep(as.Date("2020-08-01") - as.Date("2020-03-01"), 2), c(0, 100), lty=3, col="gray50")
+
+dev.off()
